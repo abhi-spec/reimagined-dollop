0d2a91b778c504ce8b0e26cfc7033590305d7ef1
abhin <abhin@DESKTOP-RSTN6S5.home>
2019-08-14 10:55:07 -0500

    new commit

---
+++ JavaProject/src/NewGitprogram.java
@@ -0,0 +1,12 @@
+
+public class NewGitprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a=2;
+   int b=4;
+   int c = a+b;
+  System.out.println("sum of these numbers:"+c);
+	}
+
+}
---
+++ JavaProject/src/NewProgram/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author abhin
+ *
+ */
+package NewProgram;
